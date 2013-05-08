b3251a8edebcca25ba91a9017af18da7e5213e4d
Alex Evsyuchenya <alex.evsyuchenya@gmail.com>
2013-05-08 12:15:21 +0300

    First commit

---
+++ 3rd party/stanford-corenlp-python/main.py
@@ -0,0 +1,12 @@
+'''
+Created on 11.3.2013
+
+@author: baydet
+'''
+
+from corenlp import *
+
+if __name__ == '__main__':
+    corenlp = StanfordCoreNLP()
+    # corenlp.parse("Parse it")
+    print 'ok'
